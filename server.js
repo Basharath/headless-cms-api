f09a91998b17cb67b247586d92829a15e27ca680
Basharath <basharath.in@gmail.com>
2021-09-15 20:45:44 +0530

    Sets up eslint and prettier

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import app from './app.js';
 import http from 'http';
+import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+// eslint-disable-next-line no-console
 server.listen(PORT, () => console.log(`Started listing on ${PORT}`));

91eb9caeb6cf130506375fa60e5d717527045a3a
Basharath <Basharath.in@gmail.com>
2021-10-09 20:52:05 +0530

    Restructures files folders for typescript

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import http from 'http';
-import app from './app.js';
+import app from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 

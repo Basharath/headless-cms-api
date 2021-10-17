399da84b152c7b6a8486c5c77c3d8cab6751d22a
Basharath <Basharath.in@gmail.com>
2021-10-17 22:01:38 +0530

    Adds post data interface

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface PostData {
+  postId: string;
+  published: string;
+  modified: string;
+  slug: string;
+  status: string;
+  type: string;
+  title: string;
+  excerpt: string;
+  content: string;
+  author: string;
+  tags: string[];
+  thumbnail: string;
+  images: string[];
+}

6ec4f5b96978d99ba915f7cec4233625e3fcfda7
Basharath <Basharath.in@gmail.com>
2021-10-29 21:20:20 +0530

    Updates users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { signIn, signUp } from '../controllers/users';
+import { signIn, signUp, changePassword } from '../controllers/users';
 
 const router = Router();
 
 router.post('/signin', signIn);
 router.post('/signup', signUp);
-// router.post('/changepassword', changePassword);
+router.post('/changepassword', changePassword);
 
 export default router;

8aada328c63d3c8160a886ea3c459092110da926
Basharath <Basharath.in@gmail.com>
2021-11-12 23:03:40 +0530

    Adds cors origin

--- src/startup/prod.ts
+++ src/startup/prod.ts
@@ -7,6 +7,15 @@ import cookieParser from 'cookie-parser';
 export default function prod(app: Application) {
   app.use(compression());
   app.use(helmet());
-  app.use(cors());
+  app.use(
+    cors({
+      origin:
+        process.env.NODE_ENV !== 'production'
+          ? 'http://localhost:3000'
+          : process.env.SITE,
+      optionsSuccessStatus: 200,
+      credentials: true,
+    })
+  );
   app.use(cookieParser());
 }

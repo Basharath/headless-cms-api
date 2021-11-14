12e1175eebfefab6a0693eed1c40d3f6515d9369
Basharath <Basharath.in@gmail.com>
2021-11-14 14:53:34 +0530

    Updates auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,9 +3,11 @@ import jwt from 'jsonwebtoken';
 import { DecodedType } from '../types';
 
 export default (req: Request, res: Response, next: NextFunction) => {
-  const { token } = req.cookies;
+  const { authorization } = req.headers;
+  const token = authorization;
 
   if (!token) return res.status(401).send('Access denied. No token provided.');
+
   try {
     const decodedData = jwt.verify(token, <string>process.env.jwtPrivateKey);
     req.user = <DecodedType>decodedData;
@@ -15,3 +17,17 @@ export default (req: Request, res: Response, next: NextFunction) => {
     return res.status(400).send('Invalid token');
   }
 };
+
+// export default (req: Request, res: Response, next: NextFunction) => {
+//   const { token } = req.cookies;
+
+//   if (!token) return res.status(401).send('Access denied. No token provided.');
+// try {
+//   const decodedData = jwt.verify(token, <string>process.env.jwtPrivateKey);
+//   req.user = <DecodedType>decodedData;
+
+//   return next();
+// } catch (err) {
+//   return res.status(400).send('Invalid token');
+// }
+// };

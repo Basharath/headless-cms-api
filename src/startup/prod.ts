04de5eb2b1c823558a69b8583273db7ca7df34fe
Basharath <Basharath.in@gmail.com>
2021-12-06 22:27:08 +0530

    Comments cors

--- src/startup/prod.ts
+++ src/startup/prod.ts
@@ -1,22 +1,21 @@
 import { Application } from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
-import cors from 'cors';
+// import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 export default function prod(app: Application) {
   app.use(compression());
   app.use(helmet());
-  app.use(
-    // cors({
-    //   origin:
-    //     process.env.NODE_ENV !== 'production'
-    //       ? 'http://localhost:3000'
-    //       : process.env.SITE,
-    //   optionsSuccessStatus: 200,
-    //   credentials: true,
-    // })
-    cors()
-  );
+  // app.use(
+  //   // cors({
+  //   //   origin:
+  //   //     process.env.NODE_ENV !== 'production'
+  //   //       ? 'http://localhost:3000'
+  //   //       : process.env.SITE,
+  //   //   optionsSuccessStatus: 200,
+  //   //   credentials: true,
+  //   // })
+  // );
   app.use(cookieParser());
 }
